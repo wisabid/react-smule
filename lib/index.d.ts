31b3a8d44e7d554261741c2ec49cd6d3436c3d07
Abid Shamim PM <abidwsi@yahoo.com>
2019-11-12 13:12:17 +0100

    feat(react-smule); Add react component

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+declare const ReactSmule: React.FunctionComponent<{
+    src: string;
+}>;
+export default ReactSmule;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReactSmule:React.FunctionComponent<{src : string}> = ({src}) => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default ReactSmule;
